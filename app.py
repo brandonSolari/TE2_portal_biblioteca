6744d18fee02cf31a97ced25a0b0c12305f55246
brandon <solariabrham@gmail.com>
2026-09-22 20:08:51 -0400

    ejercisio 2 portal biblioteca

--- app.py
+++ app.py
@@ -4,7 +4,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hola mundo"
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/libros')
+def libros():
+    return render_template('libros.html')
+
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
+
+@app.route('/logout')
+def logout():
+    return "Cierre de sesión"
 
 if __name__ == '__main__':
     app.run(debug=True)
